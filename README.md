059da215badb3b167f2932f43c901ea5f964b1f3
ferhatwi <87044532+ferhatwi@users.noreply.github.com>
2022-03-22 01:54:25 +0300

    setAuth added

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Supabase
- Internal dependency for Supabase clients
- 
+Main client for Supabase libraries. No need to add this dependency individually.
+## How to use?
+Initialize `Supabase` once with your Project ID and API Key as early as possible by invoking:
+```kotlin 
+Supabase.initialize(YOUR_PROJECT_ID, YOUR_API_KEY)
+```
--- lib/src/main/kotlin/io/github/ferhatwi/supabase/Supabase.kt
+++ lib/src/main/kotlin/io/github/ferhatwi/supabase/Supabase.kt
@@ -1,12 +1,19 @@
 package io.github.ferhatwi.supabase
 
+
 object Supabase {
 
     lateinit var PROJECT_ID: String
     lateinit var API_KEY: String
+    lateinit var AUTHORIZATION: String
 
     fun initialize(projectID: String, apiKey: String) {
         PROJECT_ID = projectID
         API_KEY = apiKey
+        AUTHORIZATION = apiKey
+    }
+
+    fun setAuth(authorization: String) {
+        AUTHORIZATION = authorization
     }
 }
